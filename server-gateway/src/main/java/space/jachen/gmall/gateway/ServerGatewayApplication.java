ec77f212846e304b2d290ff6e6eaf319d76162ab
Jachen <914158173@qq.com>
2023-03-04 14:35:39 +0800

    创建网关服务模板并完成跨域配置✌

---
+++ server-gateway/src/main/java/space/jachen/gmall/gateway/ServerGatewayApplication.java
@@ -0,0 +1,18 @@
+package space.jachen.gmall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 服务网关启动器
+ *
+ * @author JaChen
+ * @date 2023/03/04
+ */
+@SpringBootApplication
+public class ServerGatewayApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(ServerGatewayApplication.class,args);
+    }
+}
