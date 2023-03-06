6707146efffb0945ec66fa24c39130db4993fbe4
Jachen <914158173@qq.com>
2023-03-06 13:26:04 +0800

    新增品牌列表管理功能

--- service/service-product/src/main/java/space/jachen/gmall/product/controller/BaseTradeMarkController.java
+++ service/service-product/src/main/java/space/jachen/gmall/product/controller/BaseTradeMarkController.java
@@ -3,10 +3,7 @@ package space.jachen.gmall.product.controller;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import space.jachen.gmall.common.result.Result;
 import space.jachen.gmall.domain.product.BaseTrademark;
 import space.jachen.gmall.product.service.BaseTrademarkService;
@@ -19,11 +16,65 @@ import space.jachen.gmall.product.service.BaseTrademarkService;
  * @date 2023/3/6 12:54
  */
 @RestController
-@RequestMapping("/admin/product")
+@RequestMapping("/admin/product/baseTrademark")
 public class BaseTradeMarkController {
     @Autowired
     private BaseTrademarkService baseTrademarkService;
 
+
+    /**
+     * 删除品牌对象
+     * @param id  品牌Id
+     *
+     * @return Result<Object>
+     */
+    @DeleteMapping("/remove/{id}")
+    public Result<Object> removeBaseTrademarkById(@PathVariable Long id){
+        baseTrademarkService.getBaseMapper().deleteById(id);
+
+        return Result.ok();
+    }
+
+
+    /**
+     * 获取品牌对象
+     * @param id  品牌Id
+     *
+     * @return  BaseTrademark 品牌对象
+     */
+    @GetMapping("/get/{id}")
+    public Result<BaseTrademark> getBaseTrademarkById(@PathVariable Long id){
+        BaseTrademark baseTrademark = baseTrademarkService.getBaseMapper().selectById(id);
+
+        return Result.ok(baseTrademark);
+    }
+
+
+    /**
+     * 修改更新品牌对象
+     * @param baseTrademark  品牌对象
+     * @return Result<Object>
+     */
+    @PutMapping("/update")
+    public Result<Object> updateBaseTradeMark(@RequestBody BaseTrademark baseTrademark){
+        baseTrademarkService.updateById(baseTrademark);
+
+        return Result.ok();
+    }
+
+
+    /**
+     * 新增品牌对象
+     * @param baseTrademark  品牌对象
+     *
+     * @return Result<Object>
+     */
+    @PostMapping("/save")
+    public Result<Object> insertBaseTradeMark(@RequestBody BaseTrademark baseTrademark){
+        baseTrademarkService.save(baseTrademark);
+
+        return Result.ok();
+    }
 
 
     /**
@@ -32,7 +83,7 @@ public class BaseTradeMarkController {
      * @param limit  每页个数
      * @return IPage<BaseTrademark>
      */
-    @GetMapping("/baseTrademark/{page}/{limit}")
+    @GetMapping("/{page}/{limit}")
     public Result<IPage<BaseTrademark>> getBaseTrademarkPage(@PathVariable Long page, @PathVariable Long limit){
         // 封装分页条件
         IPage<BaseTrademark> baseTrademarkIPage = new Page<>(page,limit);
