0940e475fad6bce1420bd81f2e0959e0a04e5dc1
Jachen <914158173@qq.com>
2023-03-07 16:55:26 +0800

    搭建商品详情微服务模块

---
+++ service/service-item/src/main/java/space/jachen/gmall/item/ServiceItemApplication.java
@@ -0,0 +1,18 @@
+package space.jachen.gmall.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 商品详情微服务启动器
+ *
+ * @author JaChen
+ * @date 2023/3/7
+ */
+@SpringBootApplication
+public class ServiceItemApplication  {
+    public static void main(String[] args) {
+
+        SpringApplication.run(ServiceItemApplication.class);
+    }
+}
