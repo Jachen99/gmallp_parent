38c6fe321cb99ed0214ddf59bbd16295b8ceeae3
Jachen <914158173@qq.com>
2023-03-14 20:07:16 +0800

    功能完善

--- service-client/service-list-client/src/main/java/space/jachen/gmall/list/client/ListDegradeFeignClient.java
+++ service-client/service-list-client/src/main/java/space/jachen/gmall/list/client/ListDegradeFeignClient.java
@@ -1,11 +1,13 @@
 package space.jachen.gmall.list.client;
 
+import org.springframework.stereotype.Component;
 import space.jachen.gmall.common.result.Result;
 
 /**
  * @author JaChen
  * @date 2023/3/14 19:45
  */
+@Component
 public class ListDegradeFeignClient implements ListFeignClient{
     @Override
     public Result<String> incrHotScore(Long skuId) {
