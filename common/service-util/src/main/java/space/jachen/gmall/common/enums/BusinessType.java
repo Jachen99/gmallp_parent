7996ebc8d25efcf993db8ae209713eee9c2975be
Jachen <914158173@qq.com>
2023-03-15 16:49:11 +0800

    增加aop日志处理

---
+++ common/service-util/src/main/java/space/jachen/gmall/common/enums/BusinessType.java
@@ -0,0 +1,42 @@
+package space.jachen.gmall.common.enums;
+
+/**
+ *
+ * 业务操作类型
+ *
+ * @author JaChen
+ * @date 2022/12/26 11:57
+ */
+public enum BusinessType {
+
+    /**
+     * 其他类型
+     */
+    OTHER,
+
+    /**
+     * 新增
+     */
+    INSERT,
+
+    /**
+     * 修改
+     */
+    UPDATE,
+
+    /**
+     * 删除
+     */
+    DELETE,
+
+    /**
+     * 更新状态
+     */
+    STATUS,
+
+    /**
+     * 授权
+     */
+    ASSIGN
+
+}
---
+++ common/service-util/src/main/java/space/jachen/gmall/common/enums/OperatorType.java
@@ -0,0 +1,27 @@
+package space.jachen.gmall.common.enums;
+
+/**
+ *
+ * 操作人类型
+ *
+ * @author JaChen
+ * @date 2022/12/26 11:58
+ */
+public enum OperatorType {
+
+    /**
+     * 其它
+     */
+    OTHER,
+
+    /**
+     * 后台用户
+     */
+    MANAGE,
+
+    /**
+     * 手机端用户
+     */
+    MOBILE
+
+}
