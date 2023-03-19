8bb0b6edd5bc3ae26f8d596f17287d2377e94859
Jachen <914158173@qq.com>
2023-03-19 22:49:49 +0800

    提供用户地址信息远程调用接口

--- service/service-user/src/main/java/space/jachen/gmall/user/ServiceUserApplication.java
+++ service/service-user/src/main/java/space/jachen/gmall/user/ServiceUserApplication.java
@@ -2,6 +2,7 @@ package space.jachen.gmall.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @ComponentScan("space.jachen.gmall")
+@EnableFeignClients("space.jachen.gmall")
 public class ServiceUserApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceUserApplication.class, args);
