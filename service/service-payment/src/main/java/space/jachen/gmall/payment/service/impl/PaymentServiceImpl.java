9c3b36c85d6ff91b36cfde81e4e1efe8a649d348
Jachen <914158173@qq.com>
2023-03-22 19:13:57 +0800

    编写保存支付信息的接口并添加相关实体类

--- service/service-payment/src/main/java/space/jachen/gmall/payment/service/impl/PaymentServiceImpl.java
+++ service/service-payment/src/main/java/space/jachen/gmall/payment/service/impl/PaymentServiceImpl.java
@@ -1,8 +1,15 @@
 package space.jachen.gmall.payment.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import space.jachen.gmall.domain.enums.PaymentStatus;
 import space.jachen.gmall.domain.order.OrderInfo;
+import space.jachen.gmall.domain.payment.PaymentInfo;
+import space.jachen.gmall.payment.mapper.PaymentInfoMapper;
 import space.jachen.gmall.payment.service.PaymentService;
+
+import java.util.Date;
 
 /**
  * @author JaChen
@@ -10,11 +17,26 @@ import space.jachen.gmall.payment.service.PaymentService;
  */
 @Service
 public class PaymentServiceImpl implements PaymentService {
-
-
+    @Autowired
+    private PaymentInfoMapper paymentInfoMapper;
     @Override
     public void savePaymentInfo(OrderInfo orderInfo, String paymentType) {
+        QueryWrapper<PaymentInfo> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("order_id", orderInfo.getId());
+        queryWrapper.eq("payment_type", paymentType);
+        Integer count = paymentInfoMapper.selectCount(queryWrapper);
+        if(count > 0) return;
+        // 保存交易记录
+        PaymentInfo paymentInfo = new PaymentInfo();
+        paymentInfo.setCreateTime(new Date());
+        paymentInfo.setOrderId(orderInfo.getId());
+        paymentInfo.setPaymentType(paymentType);
+        paymentInfo.setOutTradeNo(orderInfo.getOutTradeNo());
+        paymentInfo.setPaymentStatus(PaymentStatus.UNPAID.name());
+        paymentInfo.setSubject(orderInfo.getTradeBody());
+        paymentInfo.setTotalAmount(orderInfo.getTotalAmount());
 
+        paymentInfoMapper.insert(paymentInfo);
     }
 
 }
