9878df3dcdc2d1e81e4361ab812a41aed6a2e821
Jachen <914158173@qq.com>
2023-03-24 00:36:11 +0800

    监听关闭订单

--- service/service-order/src/main/java/space/jachen/gmall/order/recevier/OrderReceiver.java
+++ service/service-order/src/main/java/space/jachen/gmall/order/recevier/OrderReceiver.java
@@ -15,7 +15,9 @@ import space.jachen.gmall.common.config.DeadLetterMqConfig;
 import space.jachen.gmall.common.constant.MqConst;
 import space.jachen.gmall.domain.enums.ProcessStatus;
 import space.jachen.gmall.domain.order.OrderInfo;
+import space.jachen.gmall.domain.payment.PaymentInfo;
 import space.jachen.gmall.order.service.OrderService;
+import space.jachen.gmall.payment.PaymentFeignClient;
 
 import java.io.IOException;
 import java.util.Map;
@@ -25,9 +27,12 @@ import java.util.Map;
  * @date 2023/3/21 22:40
  */
 @Component
+@SuppressWarnings("all")
 public class OrderReceiver {
     @Autowired
     private OrderService orderService;
+    @Autowired
+    private PaymentFeignClient paymentFeignClient;
 
 
 
@@ -89,34 +94,61 @@ public class OrderReceiver {
     }
 
 
-
-
-    //  监听的消息
+    /**
+     * 监听关闭订单
+     * @param orderId
+     * @param message
+     * @param channel
+     */
     @SneakyThrows
     @RabbitListener(queues = DeadLetterMqConfig.queue_dead_2)
     public void cancelOrder(Long orderId , Message message, Channel channel){
-        //  判断当前订单Id 不能为空
         try {
+            //  判断订单id 是否存在！
             if (orderId!=null){
-                //  发过来的是订单Id，那么你就需要判断一下当前的订单是否已经支付了。
-                //  未支付的情况下：关闭订单
-                //  根据订单Id 查询orderInfo select * from order_info where id = orderId
-                //  利用这个接口IService  实现类ServiceImpl 完成根据订单Id 查询订单信息 ServiceImpl 类底层还是使用的mapper
+                //  根据订单Id 查询订单对象
                 OrderInfo orderInfo = orderService.getById(orderId);
-                //  判断支付状态,进度状态
-                if (orderInfo!=null && "UNPAID".equals(orderInfo.getOrderStatus())
-                        && "UNPAID".equals(orderInfo.getProcessStatus())){
-                    //  关闭订单
-                    //  int i = 1/0;
-                    orderService.execExpiredOrder(orderId);
+                //  判断
+                if(orderInfo!=null && "UNPAID".equals(orderInfo.getOrderStatus()) && "UNPAID".equals(orderInfo.getProcessStatus())){
+                    //  关闭过期订单！ 还需要关闭对应的 paymentInfo ，还有alipay.
+                    //  orderService.execExpiredOrder(orderId);
+                    //  查询paymentInfo 是否存在！
+                    PaymentInfo paymentInfo = paymentFeignClient.getPaymentInfo(orderInfo.getOutTradeNo());
+                    //  判断 用户点击了扫码支付
+                    if(paymentInfo!=null && "UNPAID".equals(paymentInfo.getPaymentStatus())){
+
+                        //  查看是否有交易记录！
+                        Boolean flag = paymentFeignClient.checkPayment(orderId);
+                        //  判断
+                        if (flag){
+                            //  flag = true , 有交易记录
+                            //  调用关闭接口！ 扫码未支付这样才能关闭成功！
+                            Boolean result = paymentFeignClient.closePay(orderId);
+                            //  判断
+                            if (result){
+                                //  result = true; 关闭成功！未付款！需要关闭orderInfo， paymentInfo，Alipay
+                                orderService.execExpiredOrder(orderId,"2");
+                            }else {
+                                //  result = false; 表示付款！
+                                //  说明已经付款了！ 正常付款成功都会走异步通知！
+                            }
+                        }else {
+                            //  没有交易记录，不需要关闭支付！  需要关闭orderInfo， paymentInfo
+                            orderService.execExpiredOrder(orderId,"2");
+                        }
+
+                    }else {
+                        //  只关闭订单orderInfo！
+                        orderService.execExpiredOrder(orderId,"1");
+                    }
                 }
             }
-        } catch (Exception e) {
-            //  消息没有正常被消费者处理： 记录日志后续跟踪处理!
 
+        } catch (Exception e) {
+            //  写入日志...
             e.printStackTrace();
         }
-        //  手动确认消息 如果不确认，有可能会到消息残留。
+        //  手动确认
         channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
     }
 
